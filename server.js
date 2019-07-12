a62f35649e47defb14f7238e3dc3e2fc18ddd580
glosskc <glosskc@gmail.com>
2019-07-12 18:27:17 -0500

    side nav

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const session = require('express-session');
-​
 const db = require('./models');
 const routes = require('./routes');
 const passport = require('./config/passport');
