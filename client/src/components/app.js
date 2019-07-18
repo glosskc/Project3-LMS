f508b738726adf0b025e4dfcd8251a2493925aad
Samuel Fillingim <srobfillingim@gmail.com>
2019-07-17 19:57:32 -0500

    created app.js, changed contact to client, tasks to task, general client things

---
+++ client/src/components/app.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Component } from 'react';
+import { connect } from 'react-redux'
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import { Link } from 'react-router';
+
+
+import MenuItem from 'material-ui/lib/menus/menu-item';
+import Toolbar from 'material-ui/lib/toolbar/toolbar';
+import ToolbarGroup from 'material-ui/lib/toolbar/toolbar-group';
+import ToolbarTitle from 'material-ui/lib/toolbar/toolbar-title';
+import LeftNav from 'material-ui/lib/left-nav';
+
+
+injectTapEventPlugin();
+
+import ClientList from '../ClientList'
+import InputForm from '../containers/InputForm'
+import TaskForm from '../containers/TaskForm'
+import TaskList from './TaskList'
+
+export default class App extends Component {
+
+  handleToggle = () => this.setState({open: !this.props.open});
+
+  render() {
+
+    const styles = {
+      toolbarTitle: {
+        paddingLeft: 30,
+
+      },
+      leftBar: {
+        width: '17%',
+        backgroundColor: '#312c32',
+      },
+      toolbarStyle: {
+        backgroundColor: "#daad86",
+      },
+      menuItem: {
+        color: "#feffff"
+      }
+    }
+
+    console.log("props on app: ", this.props)
+    return (
+      <div>
+        <div className="container">
+          <Toolbar style={styles.toolbarStyle}>
+            <ToolbarGroup firstChild={true} float="left">
+              <ToolbarTitle text="ReactReduxCRM" style={styles.toolbarTitle}/>
+            </ToolbarGroup>
+            <ToolbarGroup float="right">
+            </ToolbarGroup>
+          </Toolbar>
+        </div>
+        <LeftNav open={this.props.open} style={styles.leftBar}>
+          <MenuItem
+            value={1}
+            primaryText="Contacts"
+            linkButton
+            containerElement={<Link to="/" />}
+            style={styles.menuItem}
+          />
+          <MenuItem
+            primaryText="Tasks"
+            linkButton
+            containerElement={<Link to="/tasks" />}
+            style={styles.menuItem}
+          />
+        <img className="logoImage" src="../../reactlogojohn.png" />
+        </LeftNav>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  console.log("state: ", state);
+  return state;
+}
+
+export default connect(mapStateToProps)(App)
---
+++ client/src/components/clientList.js
@@ -0,0 +1,77 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { removeClient } from '../actions/actions';
+
+import RaisedButton from 'material-ui/lib/raised-button';
+
+
+class ClientList extends Component {
+
+  render() {
+    const styles = {
+      listPadding: {
+        padding: "0 20px 0 0"
+      },
+      listItem : {
+        padding: "0 0 20px 20px",
+        border: "1px solid black",
+        listStyleType: 'none'
+      },
+      inputFields: {
+        width: '45%',
+        margin: "0 10px"
+      },
+      noteField: {
+        width: '95%'
+      },
+      formStyle: {
+        padding: 30
+      }
+    }
+    console.log('this is props in ClientList: ', this.props);
+    return (
+      <ul style={styles.listPadding}>
+        {
+          this.props.clients.all.map((client, index) => {
+            return (
+              <div style={styles.formStyle}>
+                <li key={client.id} style={styles.listItem}>
+                  <p>First name: {client.text.firstName}</p>
+                  <p>Last name: {clientt.text.lastName}</p>
+                  {/* <p>Occupation: {client.text.occupation}</p> */}
+                  {/* <p>Organization: {client.text.organization}</p> */}
+                  <p>Email: {client.text.email}</p>
+                  <p>Phone: {client.text.phone}</p>
+                  {/* <p>Website: {client.text.website}</p> */}
+                  <p>Address: {client.text.address}</p>
+                  <p>Notes: {client.text.notes}</p>
+                  <RaisedButton label="Remove" style={styles.submitButton} onMouseDown={() => this.props.removeClient(index)} />
+                </li>
+              </div>
+
+            )
+          })
+        }
+
+      </ul>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  // Whatever is returned will show up as props
+  console.log('mapStateToProps', state)
+  return state;
+}
+
+// Anything reutrned form this function will end up as props
+// to all of our
+
+function mapDispatchToProps(dispatch){
+
+  return bindActionCreators({ removeClient }, dispatch)
+}
+contact
+
+export default connect(mapStateToProps, mapDispatchToProps)(ClientList);
