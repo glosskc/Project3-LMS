6d39e24fba1f6a42f4d44e68ace1dd69abb7f97f
LucyQ19 <lucyq19@gmail.com>
2019-07-21 14:35:35 -0500

    added SignUp modal to SignIn.js

---
+++ client/src/components/signUpModal/SignUpModal.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+export default function SignUpModal() {
+  const [open, setOpen] = React.useState(false);
+
+  function handleClickOpen() {
+    setOpen(true);
+  }
+
+  function handleClose() {
+    setOpen(false);
+  }
+
+  return (
+    <div>
+      <Button 
+        fullWidth
+        variant="contained"
+        color="primary"  
+        onClick={handleClickOpen}
+        >
+        Sign Up
+      </Button>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Sign Up</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            To sign up for an account with LMS Scheduler, please enter your first and last name, email, and create a new password here.
+          </DialogContentText>
+            <form noValidate autoComplete="off">
+                <TextField
+                    autofocus
+                    margin="normal"
+                    id="standard-name"
+                    label="User Name"
+                    type="text"
+                    fullWidth
+                    required
+                />
+                <TextField
+                    autoFocus
+                    margin="normal"
+                    id="standard-email"
+                    label="Email Address"
+                    type="email"
+                    fullWidth
+                    required
+                />
+                <TextField
+                    autofocus
+                    margin="normal"
+                    id="standard-password-input"
+                    label="Password"
+                    type="text"
+                    fullWidth
+                    required
+                />
+          </form>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Sign Up
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import SignUpModal from '../components/signUpModal';
+
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -92,13 +94,7 @@ const useStyles = makeStyles(theme => ({
               >
                 Sign In
               </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
+              <SignUpModal />
             </form>
           </div>
         </Grid>
